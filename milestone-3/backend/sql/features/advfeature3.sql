e84c3615b662c9ce7c9564dbadc0552948da9536
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-11-26 22:43:54 -0500

    Added advanced features 3 and 5 sql

---
+++ milestone-3/backend/sql/features/advfeature3.sql
@@ -0,0 +1,16 @@
+SELECT
+    aps2.player_id,
+    aps2.name,
+    aps2.team_name,
+    aps2.pts_per_game,
+    aps_target.pts_per_game AS target_pts_per_game,
+    ABS(aps2.pts_per_game - aps_target.pts_per_game) AS points_diff
+
+FROM AdvancedPlayerStats aps_target
+JOIN AdvancedPlayerStats aps2
+    ON aps2.player_id <> aps_target.player_id
+WHERE aps_target.name = 'LeBron James'
+
+ORDER BY points_diff ASC
+LIMIT 10;
+
